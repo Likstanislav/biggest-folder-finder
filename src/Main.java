933a0638afbbb31b3f3b60def34c79b0b9b77ddd
likstanislav <likstanislav@yandex.ru>
2022-06-16 22:11:39 +0700

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPatyh = "C:/Users/Станислав/Desktop/";
-        File file = new File(folderPatyh);
-        System.out.println(getFolderSize(file));
-    }
 
-    public static long getFolderSize(File folder)
-    {
-        if(folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for(File file : files) {
-            sum +=getFolderSize(file);
-        }
-        return sum;
+        String folderPath = "C:/Users/Станислав/Desktop/Мои сайты";
+        File file = new File(folderPath);
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        long duration = (System.currentTimeMillis() - start);
+        System.out.println(duration + " ms");
     }
+//
+//    public static long getFolderSize(File folder)
+//    {
+//        if(folder.isFile()) {
+//            return folder.length();
+//        }
+//        long sum = 0;
+//        File[] files = folder.listFiles();
+//        for(File file : files) {
+//            sum +=getFolderSize(file);
+//        }
+//        return sum;
 }
