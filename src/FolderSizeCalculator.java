1f7c408b24cf4bfdba4d9d2626cb4dfaa212ad0b
likstanislav <likstanislav@yandex.ru>
2022-08-14 16:45:26 +0700

    dev

--- src/FolderSizeCalculator.java
+++ src/FolderSizeCalculator.java
@@ -1,34 +1,41 @@
 import java.io.File;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class FolderSizeCalculator extends RecursiveTask<Long> {
-    private File folder;
-    public FolderSizeCalculator(File folder) {
-        this.folder = folder;
+    private Node node;
+
+    public FolderSizeCalculator(Node node) {
+        this.node = node;
     }
 
     @Override
     protected Long compute() {
-        if(folder.isFile()) {
-            return folder.length();
+        File folder = node.getFolder();
+        if (folder.isFile()) {
+            long length = folder.length();
+            node.setSize(length);
+            return length;
         }
+
         long sum = 0;
         List<FolderSizeCalculator> subTasks = new LinkedList<>();
+
         File[] files = folder.listFiles();
-
-        for(File file : files) {
-            FolderSizeCalculator task = new FolderSizeCalculator(file);
-            task.fork(); // запустим асинхронно
+        for (File file : files) {
+            Node child = new Node(file);
+            FolderSizeCalculator task = new FolderSizeCalculator(child);
+            task.fork();
             subTasks.add(task);
+            node.addChild(child);
         }
 
-        for(FolderSizeCalculator task : subTasks) {
-            sum += task.join(); // дождёмся выполнения задачи и прибавим результат
+        for (FolderSizeCalculator task : subTasks) {
+            sum += task.join();
         }
 
+        node.setSize(sum);
         return sum;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,25 @@
 import java.io.File;
-import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    private static char[] sizeMultipliers = {'B', 'K','M','G','T'};
-    public static void main(String[] args)
-    {
-        System.out.println(getHumanReadbleSize(240640));
-        System.exit(0);
 
-        String folderPath = "C:/Users/Станислав/Desktop/Влад";
+    public static void main(String[] args) {
+        ParametersBag bag = new ParametersBag(args);
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
+
         File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
 
         long start = System.currentTimeMillis();
 
-        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
+        pool.invoke(calculator);
+        System.out.println(root);
 
         long duration = System.currentTimeMillis() - start;
-        System.out.println(duration + " milliseconds");
-    }
-
-    public static long getFolderSize(File folder)
-    {
-        if(folder.isFile()) {
-            return folder.length();
-        }
-
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for(File file : files) {
-            sum += getFolderSize(file);
-        }
-        return sum;
-    }
-
-    public static String getHumanReadbleSize(long size)
-    {
-        for(int i = 0; i < sizeMultipliers.length; i++){
-            double value = size / Math.pow(1024, i);
-            if(value < 1024) {
-                return Math.round(value) + "" +
-                        sizeMultipliers[i] +
-                        (i > 0 ? "b" : "");
-            }
-        }
-        return "Very big!";
-    }
-
-    public static long getSizeFromHumanReadble(String size)
-    {
-        HashMap<Character, Integer> char2multiplier = getMultiplier();
-        char sizeFactor = size
-                .replaceAll("[0-9\\s+]+","")
-                .charAt(0);
-        int multiplier = char2multiplier.get(sizeFactor);
-        long length = multiplier * Long.parseLong(size.replaceAll("[^0-9]", "")
-        );
-        return length;
-    }
-
-    private static HashMap<Character, Integer> getMultiplier(){
-        HashMap<Character, Integer> char2multiplier = new HashMap<>();
-        for(int i = 0; i < sizeMultipliers.length; i++)
-        {
-            char2multiplier.put(
-                    sizeMultipliers[i],
-                    (int)Math.pow(1024, i));
-        }return char2multiplier;
+        System.out.println(duration + " ms");
     }
 }
 
